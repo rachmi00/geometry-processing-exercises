59e1e4ba76ad945bd19f65297d7353e9ddf33366
rachmi00 <r.agyingi@gmail.com>
2025-02-03 14:20:07 +0100

    done exercise 2

--- exercise_01/linear_systems.py
+++ exercise_01/linear_systems.py
@@ -1,4 +1,20 @@
 import numpy as np
+import scipy as sp
+
+#Linear equations
 A = np.array([[1., 2.], [3., 4.]])
 b = np.array([-2, -3])
 print(f"inv(A)@b = {np.linalg.solve(A,b)}")
+
+#Sparce matrices
+i = np.array([0,1,1,2])
+j = np.array([0,1,2,2])
+k = np.array([1., -1., -2., 1.])
+Am = sp.sparse.csr_matrix((k, (i,j)), shape=(3,3)) #the shape tuple is the dimension of A
+print(f"A = {Am.todense()}")
+i = np.array([0,0,1,2])
+j = np.array([0,1,2,1])
+k = np.array([-2., -1., 2., 1.])
+B = sp.sparse.csr_matrix((k, (i,j)), shape=(3,3))
+print(f"A+B = {(Am+B).todense()}")
+print(f"A*B = {(Am*B).todense()}")
---
+++ exercise_01/other_libraries.py
@@ -0,0 +1,8 @@
+# import numpy as np
+# import torch
+# u = np.array([1., 2.])
+# print(f"array u={u} has type {type(u)}")
+# ut = torch.from_numpy(u)
+# print(f"array ut={ut} has type {type(ut)}")
+# un = ut.numpy()
+# print(f"array un={un} has type {type(un)}")

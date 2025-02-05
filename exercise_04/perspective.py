86851a7d78e0461eb58e1dfbf1c10a56e5969919
rachmi00 <r.agyingi@gmail.com>
2025-02-05 23:50:54 +0100

    slight change

--- exercise_04/perspective.py
+++ exercise_04/perspective.py
@@ -2,5 +2,5 @@ import gpytoolbox as gpy, polyscope as ps
 V,F = gpy.regular_square_mesh(2)
 ps.init()
 ps_square = ps.register_surface_mesh("square", V, F)
-ps.set_view_projection_mode("perspective") # switching to orthographic gives us the palin 2d view
+ps.set_view_projection_mode("orthographic") # switching to orthographic gives us the palin 2d view
 ps.show()
